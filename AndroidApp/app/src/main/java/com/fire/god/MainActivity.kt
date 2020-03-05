1392c61a60f1a1dddf22602e45c15cfdb303450d
zhoushengchen <zhoushengchen@nxin.com>
2020-03-05 18:08:45 +0800

    init project

---
+++ AndroidApp/app/src/main/java/com/fire/god/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fire.god
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
